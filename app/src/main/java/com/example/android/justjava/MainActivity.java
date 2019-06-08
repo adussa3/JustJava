3f1306cccc0bd1ec60a540316035c73d22bd170d
Anunoy Dussa <adussa3@gmail.com>
2019-06-08 11:37:22 -0400

    Added a style for the Header Text

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayPrice() {
         price = calculatePrice();
-        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
     }
 
@@ -115,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.price_text_view);
         orderSummaryTextView.setText(message);
     }
 
